5ae7107a5bd29668ca0b3f31bc7c54309c7e310b
Chamod De SIlva <chamodlakshitha12@gmail.com>
2024-03-13 12:57:08 +0530

    modified

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 
 app = Flask('__name__')
  
+#Initial route  
 @app.route('/')
 def index():
     return(render_template('index.html'))
 
+#Home route
 @app.route('/home')
 def home():
     return(render_template('home.html'))
